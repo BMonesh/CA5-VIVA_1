74683cac064a0160893f64dd3f99a182bee069be
BMonesh <141898445+BMonesh@users.noreply.github.com>
2024-05-09 15:32:37 +0530

    SecondChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [rateCount, setRateCount] = useState(0)
   const [showContent, setShowContent] = useState(false)
   const [fakeData, setFakeData] = useState([])
+  const [isFilter, setIsFilter] = useState(false)
+  const [filterData, setFilterData] = useState([])
+
+  const handleChange = () => {
+    const filteredData = fakeData.filter(user => user.title.includes("Mens"))
+    setFilterData(filteredData)
+    setIsFilter(!isFilter)
+  }
 
   const handleClick = async() => {
     setCount(count+1)
@@ -14,15 +23,23 @@ function App() {
     if (count === 0){
       const datas = await axios.get("https://fakestoreapi.com/products");
       setFakeData(datas.data);
-      console.log(datas.data)
+      console.log(datas.data);
+      console.log(fakeData)
     }
     if (count === 1){
       setShowContent(!showContent)
+      fakeData.map((data) => {
+        if (data.rating.rate >= 4){
+          setRateCount(rateCount + 1)
+        }
+      })
+      document.cookie = `rate=${rateCount}; expires=Thu, 18 Dec 2030 12:00:00 UTC`
     }
     if (count === 2){
       setShowContent(!showContent)
       setCount(0)
       setFakeData([])
+      document.cookie = 'rate=; expires=Thu, 01 Jan 1970 00:00:00 UTC'
     }
   }
 
@@ -30,14 +47,34 @@ function App() {
     <React.Fragment>
       <button onClick={()=>handleClick()}>Click me!</button>
       <div className="fake-data">
-        {showContent && fakeData.map((data) => {
-          return (
-            <div key={data.id} >
-              <h3>{data.category}</h3>
-              <p>{data.image}</p>
+        {showContent ? ( 
+          <React.Fragment>
+            <div className='headers'>
+              <label>
+                Filter
+                <select name="Filter" id="filter-id" onChange={handleChange}>
+                  <option value="All">All</option>
+                  <option value="Mens">Mens</option>
+                </select>
+              </label>
+              {isFilter ? filterData.sort((a, b) => (a.price > b.price ? 1 : -1) && (a.rating.rate > b.rating.rate ? 1 : -1)).map((data) => (
+                <div key={data.id}>
+                  <h2>Tile: {data.title}</h2>
+                  <p>Price: {data.price}</p>
+                  <p>Rating: {data.rating.rate}</p>
+                </div>
+              )) :
+                fakeData.sort((a, b) => (a.price > b.price ? 1 : -1) && (a.rating.rate > b.rating.rate ? 1 : -1)).map((data)=>(
+                  <div key={data.id}>
+                    <h2>Title: {data.title}</h2>
+                    <p>Price: {data.price}</p>
+                    <p>Rating: {data.rating.rate}</p>
+                  </div>
+                ))
+              }
             </div>
-          )
-        })}
+          </React.Fragment>
+        ) : null}
       </div>
     </React.Fragment>
   )
